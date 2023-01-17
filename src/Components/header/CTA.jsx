fbd30322e4048ce2af0f366725712228e1d1657b
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-01-17 22:08:36 +0100

    add linkedin button

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import CV from '../../assets/CV.pdf';
+import React from "react";
+import CV from "../../assets/CV.pdf";
 
 function CTA() {
   return (
-    <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
-      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-      </div>
-  )
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a
+        href="#contact"
+        className="btn btn-primary"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Let's Talk
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ervin-kacar-bbb801223/"
+        className="btn btn-primary"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LinkedIn
+      </a>
+    </div>
+  );
 }
 
-export default CTA
+export default CTA;
