a801a58a8c2f31889ad38bb7dceacbe7a7146f19
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-08-24 08:36:20 +0200

    Fix bugs

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -85,7 +85,7 @@ function Experience() {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>Phyton</h4>
+                <h4>Python</h4>
                 <small className="text-light">Basic</small>
               </div>
             </article>
